1da399ed13d0b3490cc7583791432ffffe3c29d9
Assil28 <assil.dkhil28@gmail.com>
2025-01-09 09:59:27 +0100

    Members page , footer and header Add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
+import { MembresComponent } from './membres/membres.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent,HomeComponent,AcceuilComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent,HomeComponent,AcceuilComponent,MembresComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component'; // Standalone component
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { MembresComponent } from './membres/membres.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    AppComponent, // Import the standalone component
+    AppComponent,
+    RouterModule.forRoot(routes),
+    HomeComponent,
+    MembresComponent
+     // Import the standalone component
   ],
   bootstrap: [AppComponent]
 })
